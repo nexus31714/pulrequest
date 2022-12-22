759ace1a3a28e2b0c2414e3de5004fc91264d2d5
muho497 <114869487+muho497@users.noreply.github.com>
2022-12-22 11:25:31 +0300

    Update Reverse.py

--- Reverse/Reverse.py
+++ Reverse/Reverse.py
@@ -60,3 +60,21 @@ cv2.namedWindow('test', 0)
 cv2.imwrite('output.jpg', inMat)
 cv2.imshow('test', inMat)
 cv2.waitKey(0)
+
+#manuel histogram hesaplama
+histogramDizisi = np.zeros(256) #Histogram hesabı için 0 'lardan oluşan boş bir dizi oluşturdum.
+yukseklik,genislik=kartalResmi.shape #Görüntünün boyutları alındı çünkü for döngüsünde sınırları belirlememiz gerekiyordu.
+
+for i in range(yukseklik):
+    for j in range(genislik):
+        pikselDegeri=kartalResmi[i][j] #Görüntünün  piksel değerleri sıra ile alındı.
+        histogramDizisi[pikselDegeri]= histogramDizisi[pikselDegeri] + 1 #okunan değerin index ine gidilip 1 artırıldı.
+
+
+np.set_printoptions(suppress=True) #histogram dizisini yazdırırken üst biçiminde yazıyordu onu engellemek için yapıldı.Örn:2.97351e+05  nin yerine 297351 yazımı gibi daha saegözükmesi için
+
+print(histogramDizisi)
+print("\nHistogram dizisinin en çok tekrar eden değeri: ",np.max(histogramDizisi))
+
+
+cv2.waitKey()
